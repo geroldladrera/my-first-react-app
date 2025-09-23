8ede3de21046adc5377df11a3fdfb97997180e45
geroldladrera <geroldladrera@gmail.com>
2025-09-23 13:11:17 +0800

    set like and dislike state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "💖" : "🩶"}
+      </button>
     </div>
   );
 };
